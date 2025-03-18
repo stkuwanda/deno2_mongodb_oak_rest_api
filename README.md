9ff6d4b1404680c768d7b8aa17d4948791272e60
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-18 11:06:47 +0200

    initial project setup, dependencies setup, envconfig setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# REST API with Deno2, Oak and MongoDB
+
+## Overview
+
+This project is built with [Deno](https://deno.land/) and uses **MongoDB** as the database. Follow the steps below to set up and run the project locally.
---
+++ src/debs.ts
@@ -0,0 +1,6 @@
+export { Application } from 'jsr:@oak/oak/application';
+export { Router } from 'jsr:@oak/oak/router';
+export type { RouterContext } from 'jsr:@oak/oak/router';
+export { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
+export { MongoClient } from 'jsr:@db/mongo/client';
+export { ObjectId } from 'jsr:@db/mongo';
